e8b2633d349f56f6b55c8ff10a313bc77b60880b
Raman Samuilau <raman.samuilau1@ibm.com>
2022-06-04 14:37:21 +0200

    feat: add cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
 export const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_value1 = "value1";
+  process.env.RSS_value2 = "value2";
+  const result = [];
+
+  for (const env in process.env) {
+    if (env.startsWith("RSS_")) {
+      result.push(`${env}=${process.env[env]}`);
+    }
+  }
+  console.log(result.join("; "));
 };
+
+parseEnv();
