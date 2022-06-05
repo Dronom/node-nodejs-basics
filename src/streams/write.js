41db8b7f360249d5d7b65c1cd90b545ed0cd01ad
Raman Samuilau <raman.samuilau1@ibm.com>
2022-06-05 20:21:58 +0200

    feat: done streams tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const stream = fs.createWriteStream(
+    path.join(__dirname, "/files", "fileToWrite.txt")
+  );
+
+  process.stdout.write(
+    "Please input your text. When you want to exit - press Ctrl+C \n"
+  );
+  process.stdin.on("data", (data) => {
+    stream.write(data);
+  });
 };
+
+write();
