0ab2a4c0f8ad6173b66bb13d3c569681975539b0
Raman Samuilau <raman.samuilau1@ibm.com>
2022-06-05 20:56:52 +0200

    feat: done zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,24 @@
+import fs from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream";
+import path from "path";
+import { fileURLToPath } from "url";
+
 export const compress = async () => {
-    // Write your code here 
+  const __fileName = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__fileName);
+
+  const gzip = createGzip();
+  const readStream = fs.createReadStream(
+    path.join(__dirname, "files", "fileToCompress.txt")
+  );
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, "files", "archive.gz")
+  );
+
+  pipeline(readStream, gzip, writeStream, (err) => {
+    console.log("aa", err);
+  });
 };
+
+compress();
